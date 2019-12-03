be29b63532b80813e50bb079e47e548dca3caa20
Otto Cedeno <otto@ottostacos.com>
2019-12-03 03:51:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,14 @@
 class Song
 
   @@count = 0
-  
+
   attr_reader :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
     @artist  = artist
     @genre = genre
+    @@count += 1
   end
 
 end
