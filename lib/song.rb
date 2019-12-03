32b2a7b2818e42de40a6f733a00ff88f7f656e67
Otto Cedeno <otto@ottostacos.com>
2019-12-03 04:05:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,7 @@ class Song
   def self.artists
     unique_artists = []
     @@artists.eact do |artist|
-      unique_artists << artist if unique_artists.include?(artist)
+      unique_artists << artist if !unique_artists.include?(artist)
     end
   end
 end
